dc598d79fd3af3d1db719b8477470b2448b252b0
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2014-07-08 22:25:48 -0400

    add user destroy mutation and spec.

---
+++ spec/lib/ikat/user/destroy_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe UserDestroy do
+  let(:user) { create :user }
+
+  context 'with permitted user' do
+    let(:action) do
+      UserDestroy.run(
+        current_user: user,
+        user: {
+          id: user.id.to_s,
+        }
+      )
+    end
+
+    it { expect(action.success?).to eql true }
+    it { expect(action.result).to eql nil }
+  end
+
+  context 'with unpermitted user' do
+    let(:other) { create :user }
+
+    let(:action) do
+      UserDestroy.run(
+        current_user: other,
+        user: {
+          id: user.id.to_s,
+        }
+      )
+    end
+
+    it { expect(action.success?).to eql false }
+    it { expect(action.errors.symbolic).to eql ({ 'current_user' => :unauthorized }) }
+  end
+
+end
