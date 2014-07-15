2e35ef7ca1b6dcb4fc645cbb78cbb3f684855b1b
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2014-07-15 01:05:36 -0400

    added some controller and ui work.

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,5 +1,14 @@
 class StoresController < ApplicationController
+  before_filter :current_store
+
   def show
-    d { 'store!' }
+    @products = @current_store.products.order('created_at desc')
+  end
+
+  protected
+
+  def current_store
+    @current_store = Store.where(domain: params[:store_domain]).first
+    @current_store
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product
   include Mongoid::Userstamps
   include Mongoid::Paperclip
   include Canable::Ables
+  include Rails.application.routes.url_helpers
 
   field :name,     type: String
   field :link,     type: String
@@ -11,16 +12,19 @@ class Product
   field :currency, type: Symbol
   field :expired,  type: Boolean, default: false
   field :original_image, type: String
+  field :slug,     type: String
 
   has_mongoid_attached_file :photo,
     path:          'products/:attachment/:id/:style.:extension',
     styles: {
-      original: ['1920x1680>', :jpg],
-      large:    ['500x500>',   :jpg],
-      medium:   ['250x250',    :jpg],
+      original: ['1000x1000#', :jpg],
+      large:    ['500x500#',   :jpg],
+      medium:   ['250x250#',    :jpg],
       small:    ['100x100#',   :jpg]
     },
     convert_options: { all: '-background white -flatten +matte' }
+
+  # process_in_background :photo
 
   validates :name,     presence: true
   validates :link,     presence: true
@@ -29,6 +33,7 @@ class Product
   validates :store,    presence: true
   validates :category, presence: true
   validates :original_image, presence: true
+  validates :slug,     presence: true
 
   validates_attachment :photo,
     presence: true,
@@ -36,6 +41,8 @@ class Product
 
   belongs_to :store
   belongs_to :category
+
+  before_validation :create_slug!
 
   has_and_belongs_to_many :lists
 
@@ -63,11 +70,36 @@ class Product
     "#{self.class.currencies[self.currency]}#{(self.price % 1 != 0 ? self.price : self.price.to_i )}"
   end
 
+  def permalink
+    product_path(store_domain: self.store.domain, product_slug: self.slug)
+  end
+
   class << self
     def currencies
       {
         usd: '$'
       }
+    end
+  end
+
+  protected
+
+  def create_slug!
+    if self.name
+      str = self.name
+
+      str = str.gsub(/[^a-zA-Z0-9 ]/,"")
+      str = str.gsub(/[ ]+/," ")
+      str = str.gsub(/ /,"-")
+      str = str.downcase
+
+      others = self.class.where(store_id: self.store_id, slug: str).count
+
+      if others > 1
+        str = str + "-#{others}"
+      end
+
+      self.slug = str
     end
   end
 end
