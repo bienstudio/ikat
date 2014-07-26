39541da69ebe0aa887b84e9c76d413334e7d7907
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2014-07-26 00:48:39 -0400

    make ui a grid instead of masonry.

--- app/uploader/avatar_uploader.rb
+++ app/uploader/avatar_uploader.rb
@@ -1,15 +1,19 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
+  def default_url
+    ActionController::Base.helpers.asset_path('avatar.png')
+  end
+
   version :large do
-    process resize_to_limit: [1000, 1000]
+    process resize_to_fill: [1000, 1000]
   end
 
   version :medium do
-    process resize_to_limit: [500, 500]
+    process resize_to_fill: [500, 500]
   end
 
   version :small do
-    process resize_to_limit: [250, 250]
+    process resize_to_fill: [250, 250]
   end
 end
