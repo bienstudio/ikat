06410bd2fc748be7634a755eac6023f45d0ecc2d
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2014-08-02 01:00:15 -0400

    added ProductFlux mutation and specs.

--- lib/ikat/product/flux.rb
+++ lib/ikat/product/flux.rb
@@ -1,3 +1,5 @@
+# "Flux" the membership of a Product in a List. If it's already in the list,
+#   remove it. If it isn't in the list, add it.
 class ProductFlux < IkatMutation
   required do
     model :current_user, class: User
@@ -6,10 +8,17 @@ class ProductFlux < IkatMutation
   end
 
   def execute
-    if list_item = ListItem.where(product: product, list: list).first
-      list.remove!(product)
+    if product.in_list?(list)
+      i = list.remove!(product, current_user)
+
+      if i == true
+        i = nil
+      end
     else
-      list.add!(product)
+      i = list.add!(product, current_user)
     end
+
+    # This shouldn't be necessary...
+    return [product, i]
   end
 end
