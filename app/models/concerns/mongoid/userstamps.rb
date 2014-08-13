d3b3e14d535f4a13a0e274416022c1be77868aa0
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2014-08-13 00:01:12 -0700

    make userstamps accept other types besides users (fixes #26).

--- app/models/concerns/mongoid/userstamps.rb
+++ app/models/concerns/mongoid/userstamps.rb
@@ -3,24 +3,29 @@ module Mongoid
     extend ActiveSupport::Concern
 
     included do
+      field :created_by_type, type: String
       field :created_by, type: BSON::ObjectId
+
+      field :updated_by_type, type: String
       field :updated_by, type: BSON::ObjectId
     end
 
     def creator
-      self.created_by ? User.find(self.created_by) : nil
+      self.created_by ? self.created_by_type.constantize.find(self.created_by) : nil
     end
 
-    def creator=(u)
-      self.created_by = u.id
+    def creator=(o)
+      self.created_by = o.id
+      self.created_by_type = o.class.to_s
     end
 
     def updater
-      self.updated_by ? User.find(self.updated_by) : nil
+      self.updated_by ? self.updated_by_type.constantize(self.updated_by) : nil
     end
 
-    def updater=(u)
-      self.updated_by = u.id
+    def updater=(o)
+      self.updated_by = o.id
+      self.updated_by_type = o.class.to_s
     end
   end
 end
