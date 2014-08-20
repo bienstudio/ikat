df537edd39c464a335673d0dbd886058c3d026a9
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2014-08-19 23:48:43 -0400

    integrate helpful.io (closes #11).

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def helpful_link(text = "Support")
+    if current_user
+      mail_to "ikat@helpful.io", text, "data-helpful" => "ikat", "data-helpful-modal" => "on", "data-helpful-name" => current_user.name, "data-helpful-email" => current_user.email
+    else
+      mail_to "ikat@helpful.io", text, "data-helpful" => "ikat", "data-helpful-modal" => "on"
+    end
+  end
 end
