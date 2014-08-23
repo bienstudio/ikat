b81b75942321fe0fa038d2d3e3c6f06c8846de2c
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2014-08-23 12:02:45 -0400

    authentication for rails_admin.

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,24 +1,16 @@
 RailsAdmin.config do |config|
-
-  ### Popular gems integration
-
-  ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
-
-  ## == Cancan ==
-  # config.authorize_with :cancan
-
-  ## == PaperTrail ==
-  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
-
-  ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
+  config.authenticate_with do
+    if current_user
+      redirect_to '/' unless current_user.admin?
+    else
+      redirect_to '/login'
+    end
+  end
+  config.current_user_method { current_user }
 
   config.actions do
-    dashboard                     # mandatory
-    index                         # mandatory
+    dashboard
+    index
     new
     export
     bulk_delete
@@ -26,9 +18,5 @@ RailsAdmin.config do |config|
     edit
     delete
     show_in_app
-
-    ## With an audit adapter, you can add:
-    # history_index
-    # history_show
   end
 end
