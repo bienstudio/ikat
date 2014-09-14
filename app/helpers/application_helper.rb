cac1421513d98186dea9f3144b7def425850f32c
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2014-09-14 00:08:40 -0400

    pluralize counts. make outgoing links a helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,16 @@ module ApplicationHelper
       mail_to "ikat@helpful.io", text, "data-helpful" => "ikat", "data-helpful-modal" => "on"
     end
   end
+
+  def outgoing_link_to(text, url, options = {})
+    classes = 'outgoing '
+
+    if options[:class]
+      classes = classes + options[:class]
+    end
+
+    link_to url, class: classes do
+      "#{text} <i class='ss-icon'>out</i>".html_safe
+    end
+  end
 end
