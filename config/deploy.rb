f68b80acbd0459be201e5a83fedefe3dd516ee37
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2014-09-15 20:50:47 -0400

    deploy process cleanup.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,11 @@
 lock '3.2.1'
 
 set :application, 'ikat'
+
+set :scm, :git
 set :repo_url, 'git@github.com:eturk/ikat.git'
 
 set :deploy_to, '/var/www/ikat'
-
-set :scm, :git
 
 set :rbenv_custom_path, '/home/ubuntu/.rbenv'
 set :rbenv_ruby, '2.1.2'
@@ -20,7 +20,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      execute :mkdir, '-p', "#{ release_path }/tmp"
+      execute :mkdir, '-p', "#{release_path}/tmp"
       execute :touch, release_path.join('tmp/restart.txt')
     end
   end
@@ -30,14 +30,6 @@ namespace :deploy do
   after :publishing, :restart
 
   after :publishing, 'deploy:assets:precompile'
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      within release_path do
-        execute :rake, 'assets', 'precompile'
-      end
-    end
-  end
 
   desc 'Set the RACK_ENV and RAILS_ENV environment variables.'
   task :setup_env do
@@ -51,7 +43,7 @@ namespace :deploy do
   end
 
   namespace :assets do
-    desc 'Precompile assets'
+    desc 'Precompile assets for the correct environment.'
     task :precompile do
       on roles(:app) do
         within release_path do
